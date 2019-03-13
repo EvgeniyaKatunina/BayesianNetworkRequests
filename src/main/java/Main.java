9a6be1c1c0c079d46b8199be852fedac827f9b0a
Evgeniya Katunina <evgeniya-katunina@yandex.ru>
2019-03-13 10:30:27 +0300

    Add wrapper for IO.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+import java.io.*;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); PrintWriter pw =
+                new PrintWriter(new OutputStreamWriter(System.out));) {
+            for (; ; ) {
+                String request = br.readLine();
+                pw.println(calculateProbability(request));
+            }
+        }
+    }
+
+    public static double calculateProbability(String request) {
+        return 0;
+    }
 }
